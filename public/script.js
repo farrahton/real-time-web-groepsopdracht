14a117d245eacacee763226547ab30c2203a5d04
timmit147 <timmeeuwsen@hotmail.nl>
2022-04-19 13:00:29 +0200

    code combined

--- public/script.js
+++ public/script.js
@@ -13,20 +13,23 @@ document.querySelector('form').addEventListener('submit', event => {
 })
 
 socket.on('message', message => {
-  var elem = document.createElement('div');
-  var caracters = document.querySelector("#caracters");
-  caracters.appendChild(elem);
+  var elem = document.createElement('img');
+  var characters  = document.querySelector("#characters");
+  var character = new Array ("/images/amongus.webp", "/images/charmander.png", "/images/dino.png", "/images/dory.png", "/images/flappy.png", "/images/iron.png", "/images/kirby.png", "/images/link.png", "/images/minecraft.webp", "/images/kirby.png", "/images/minion.png", "/images/pacman.png", "/images/perry.png", "/images/rainbowcat.png", "/images/rat.png", "/images/ray.webp", "/images/sonic.png", "/images/stitch.png")
+  var randomize = Math.floor(Math.random() * character.length);
+  elem.src = character[randomize];
+  characters.appendChild(elem);
   messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
   messages.scrollTop = messages.scrollHeight
 })
 
-// getting a random character
+// // getting a random character
 
-window.onload  = pickCharacter
+// window.onload  = pickCharacter
+// // var randomize = Math.floor(Math.random() * character.length);
 
-var character = new Array ("/images/amongus.webp", "/images/charmander.png", "/images/dino.png", "/images/dory.png", "/images/flappy.png", "/images/iron.png", "/images/kirby.png", "/images/link.png", "/images/minecraft.webp", "/images/kirby.png", "/images/minion.png", "/images/pacman.png", "/images/perry.png", "/images/rainbowcat.png", "/images/rat.png", "/images/ray.webp", "/images/sonic.png", "/images/stitch.png")
+// var character = new Array ("/images/amongus.webp", "/images/charmander.png", "/images/dino.png", "/images/dory.png", "/images/flappy.png", "/images/iron.png", "/images/kirby.png", "/images/link.png", "/images/minecraft.webp", "/images/kirby.png", "/images/minion.png", "/images/pacman.png", "/images/perry.png", "/images/rainbowcat.png", "/images/rat.png", "/images/ray.webp", "/images/sonic.png", "/images/stitch.png")
 
-function pickCharacter() {
-  var randomize = Math.floor(Math.random() * character.length);
-  document.getElementById("character").src = character[randomize];
-} 
+// function pickCharacter() {
+//   document.getElementById("character").src = character[randomize];
+// } 
